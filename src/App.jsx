b42c498c3980ece088d4091214c1c6c494ca79cf
almohanad khalil <almohanedkhalil@gmail.com>
2025-10-08 21:55:34 +0300

    working on the nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <header>
+        <div className="navbar">
+          <div className="nav-logo">AL.CO</div>
+          <input type="checkbox" id="nav-menu-checkbox" className="nav-menu-checkbox"/>
+          <div className="nav-menu">
+            <a href="" className="menu-item">
+              Home
+            </a>
+            <a href="" className="menu-item">
+              About
+            </a>
+            <a href="" className="menu-item">
+              Popular Products
+            </a>
+            <a href="" className="menu-item">
+              Ratings
+            </a>
+          </div>
+          <button className="contact-btn btn">CONTACT US</button>
+          <div className="nav-cart"></div>
+          <label htmlFor="nav-menu-checkbox" className="nav-menu-toggle btn">
+            <span className="nav-menu-icon"></span>
+          </label>
+        </div>
+      </header>
+      <main>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          <span>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo, esse
+            delectus. A repudiandae amet maiores omnis cum. Asperiores autem
+            necessitatibus deserunt sit velit quos voluptatibus, rem ullam
+            distinctio ipsum sapiente? <br />
+          </span>
+          <span>
+            Molestiae ipsum quod excepturi obcaecati earum exercitationem
+            veniam? Veniam temporibus sapiente quos doloribus nobis eaque? Iure,
+            distinctio! Sed unde qui commodi libero placeat quo adipisci quis
+            similique quia iusto! Vel. <br />
+          </span>
+          <span>
+            Error eligendi nulla, distinctio nam illo consectetur voluptatibus
+            ullam esse nobis deserunt ducimus animi quidem libero, placeat
+            laudantium consequatur? Iusto, nobis eos. Magnam, ipsa numquam
+            assumenda ea corporis mollitia magni. <br />
+          </span>
+          <span>
+            Atque, accusantium. Nesciunt, nobis, consequuntur iure vel eligendi
+            saepe dolorem possimus a odit dignissimos, nostrum sunt. Dolore
+            consectetur dolorum, labore cum possimus optio fuga iste, tempora,
+            distinctio et veritatis nobis! <br />
+          </span>
+          <span>
+            Neque ad aliquid recusandae nobis cupiditate harum quisquam,
+            accusantium nihil repellat iusto porro cumque placeat dolores
+            obcaecati saepe exercitationem assumenda sunt repudiandae officia
+            doloremque fugiat. Voluptas veritatis nam dignissimos commodi.
+          </span>
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
